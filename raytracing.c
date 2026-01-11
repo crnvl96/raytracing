184f5480d659b87555a8c1ead68231179f12fbae
Adran Carnavale <adran.carnavale@gmail.com>
2026-01-11 18:07:18 -0300

    Fill window with a small rect

--- raytracing.c
+++ raytracing.c
@@ -1,7 +1,11 @@
-#include <SDL2/SDL.h>
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_rect.h"
+#include "SDL2/SDL_surface.h"
+#include "SDL2/SDL_video.h"
 
 #define WIDTH 1600
 #define HEIGHT 1200
+#define COLOR_WHITE 0xffffff
 
 int main() {
   SDL_Init(SDL_INIT_VIDEO);
@@ -10,5 +14,10 @@ int main() {
       SDL_CreateWindow("Raytracing", SDL_WINDOWPOS_CENTERED,
                        SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
 
-  SDL_Delay(5000);
+  SDL_Surface *surface = SDL_GetWindowSurface(window);
+  SDL_Rect rect = (SDL_Rect){200, 200, 200, 200};
+  SDL_FillRect(surface, &rect, COLOR_WHITE);
+  SDL_UpdateWindowSurface(window);
+
+  SDL_Delay(2000);
 }
